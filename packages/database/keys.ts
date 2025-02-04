24d75ad6f9552c857e8a164b93a7b1c5d6af9fc4
Mohamed Ezzat <dev.moezzat@gmail.com>
2025-02-04 23:04:18 +0200

    feat: migrate to supabase

--- packages/database/keys.ts
+++ packages/database/keys.ts
@@ -5,8 +5,10 @@ export const keys = () =>
   createEnv({
     server: {
       DATABASE_URL: z.string().min(1).url(),
+      DIRECT_URL: z.string().min(1).url(),
     },
     runtimeEnv: {
       DATABASE_URL: process.env.DATABASE_URL,
+      DIRECT_URL: process.env.DIRECT_URL,
     },
   });
