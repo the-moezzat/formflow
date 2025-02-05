7e08ee7e9ae324f6d98e4976b7fb2befd612089e
Mohamed Ezzat <dev.moezzat@gmail.com>
2025-02-05 02:21:57 +0200

    feat: add local model ollama

--- packages/ai/lib/models.ts
+++ packages/ai/lib/models.ts
@@ -1,6 +1,8 @@
 import { createOpenAI } from '@ai-sdk/openai';
+import { createOllama } from 'ollama-ai-provider';
 import { keys } from '../keys';
 
+const ollama = createOllama();
 const openai = createOpenAI({
   apiKey: keys().OPENAI_API_KEY,
   compatibility: 'strict',
@@ -8,5 +10,6 @@ const openai = createOpenAI({
 
 export const models = {
   chat: openai('gpt-4o-mini'),
+  local: ollama('llama3.1:latest'),
   embeddings: openai('text-embedding-3-small'),
 };
